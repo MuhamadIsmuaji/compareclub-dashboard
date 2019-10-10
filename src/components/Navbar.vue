cd995914bd1300131acb0da80db201a078bb0ab7
MuhamadIsmuaji <muhamadismuaji@gmail.com>
2019-10-10 16:32:31 +0700

    navbar #inprogress

---
+++ src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item navbar-item-logo" href="#">
+        <img src="/img/compareclub-logo.svg" width="112" height="28">
+      </a>
+
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a href="#" class="navbar-item" v-for="menu in menus" :key="menu">{{ menu }}</a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            <i class="fas fa-user-circle fa-lg user-avatar"></i>&nbsp;Hi, Jennifer
+          </a>
+
+        <div class="navbar-dropdown is-right">
+          <a class="navbar-item">
+            Profile
+          </a>
+          <a class="navbar-item">
+            Other Menu
+          </a>
+          <hr class="navbar-divider">
+          <div class="navbar-item">
+            Logout
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menus: ['Groceries', 'Fuel', 'Insurance', 'Credit', 'Bills', 'About Us', 'Blog']
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
+import './assets/styles/main.scss';
+
+// fontawesome
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+
 import Vue from 'vue'
 import App from './App.vue'
+
+// components
+import Navbar from './components/Navbar';
+Vue.component('Navbar', Navbar);
 
 Vue.config.productionTip = false
 
