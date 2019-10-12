187ad11abc505ce0ab38a61ed5c662b027ffb038
MuhamadIsmuaji <muhamadismuaji@gmail.com>
2019-10-12 07:22:56 +0700

    Tabs style and test for manipulating view

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,42 @@
   <div>
     <navbar></navbar>
     <div class="main-content-container">
-        <div class="panel">
-          <div class="panel-heading coc-panel-heading">Hello</div>
-          <div class="panel-block coc-panel-block coc-panel-block-gap">
-            <p class="control has-icons-left">
-              <input class="input is-small" type="text" placeholder="search">
-              <span class="icon is-small is-left">
-                <i class="fas fa-search" aria-hidden="true"></i>
-              </span>
-            </p>
-          </div>
+      <div class="tabs is-boxed is-fullwidth coc-tabs coc-dashboard-tabs">
+        <ul>
+          <li :class="tabActive == 'gauge' ? 'is-active' : ''" @click="tabActive = 'gauge'">
+            <a><span class="icon is-large"><i class="fas fa-image fa-2x" aria-hidden="true"></i></span></a>
+          </li>
+          <li :class="tabActive == 'other' ? 'is-active' : ''" @click="tabActive = 'other'">
+            <a><span class="icon is-large"><i class="fas fa-image fa-2x" aria-hidden="true"></i></span></a>
+          </li>
+        </ul>
+      </div>
+      <!-- tab contents -->
+      <div :class="'panel ' + (tabActive == 'gauge' ? 'coc-tab-content-active' : 'coc-tab-content')">
+        <div class="panel-block coc-panel-block coc-panel-block-gap">
+          <h1>Gauge Content</h1>
         </div>
+      </div>
+      <div :class="'panel ' + (tabActive == 'other' ? 'coc-tab-content-active' : 'coc-tab-content')">
+        <div class="panel-block coc-panel-block coc-panel-block-gap">
+          <h1>Other Content</h1>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      tabActive: 'gauge',
+    }
+  },
+  methods: {
+    test() {
+      
+    }
+  }
+}
+</script>
---
+++ src/components/PanelBackup.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="panel">
+      <div class="panel-heading coc-panel-heading">Hello</div>
+      <div class="panel-block coc-panel-block coc-panel-block-gap">
+        <p class="control has-icons-left">
+          <input class="input is-small" type="text" placeholder="search">
+          <span class="icon is-small is-left">
+            <i class="fas fa-search" aria-hidden="true"></i>
+          </span>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
